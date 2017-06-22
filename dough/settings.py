538e2064c96a15bbb3a68c543f3c01489d736e50
Hasse <hanneshermansson0000@gmail.com>
2017-06-22 13:11:13 +0200

    minor fixes to ui for production

--- dough/settings.py
+++ dough/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = safe['sk']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -123,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = '/home/dough/static'
 STATIC_URL = '/static/'
 
 #login
